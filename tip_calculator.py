4f192ace86a10f94ad2d132f3b86c44c06feb2c0
Nduduzo Ntshangase <nduduzontshangase@gmail.com>
2022-03-28 17:05:39 +0200

    add tip percentage amount and tip calculation

--- tip_calculator.py
+++ tip_calculator.py
@@ -3,3 +3,9 @@
 print("Welcome to the tip calculator")
 #Collect total of the food bill
 bill = float(input("What was the total bill?\n"))
+
+# Tip percent to be given to waiter
+tip = int(input("What percentage tip would you like to give? 10, 12, 15?\n"))
+
+tip_amount = (tip / 100) * bill
+total_bill = tip_amount + bill
