0fdf6bac20106cc28f8416dc6c0cc6bf3120086c
Nduduzo Ntshangase <nduduzontshangase@gmail.com>
2022-03-28 17:08:01 +0200

    add people to split bill and format

--- tip_calculator.py
+++ tip_calculator.py
@@ -3,3 +3,15 @@
 print("Welcome to the tip calculator")
 #Collect total of the food bill
 bill = float(input("What was the total bill?\n"))
+
+
+
+
+
+people = int(input("How many people to split the bill?\n"))
+
+amount = total_bill / people
+
+final_amount = "{:.2f}".format(amount)
+
+print(f"Each person should pay {final_amount}")
